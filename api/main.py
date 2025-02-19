f8ebd641a2cf52504f96f6f4092519c136b28cf6
phongcheat-phus <phongcheat.phus@gmail.com>
2025-02-19 08:55:27 +0700

    create python api 3 method 1.read_root 2.read_item 3.create_item

--- api/main.py
+++ api/main.py
@@ -1,29 +1,14 @@
 from fastapi import FastAPI
-import psycopg2
-import os
 
 app = FastAPI()
-
-# ดึงค่าตัวแปรจาก .env
-DB_HOST = os.getenv("DB_HOST", "database")
-DB_USER = os.getenv("DB_USER", "OSdH5f")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "bq0@HX^Tpq")
-DB_DATABASE = os.getenv("DB_DATABASE", "db_directus")
-
 @app.get("/")
-def hello():
+def read_root():
     return {"message": "Hello, World!"}
 
-@app.get("/db")
-def test_db_connection():
-    try:
-        conn = psycopg2.connect(
-            dbname=DB_DATABASE,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            host=DB_HOST,
-            expose=5432
-        )
-        return {"message": "Connected to PostgreSQL!"}
-    except Exception as e:
-        return {"error": str(e)}
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.post("/items/")
+def create_item(item: dict): 
+    return {"message": "item created", "item": item}
