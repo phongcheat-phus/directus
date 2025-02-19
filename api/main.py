db268ff18843afa150c62e1d884cc8ae513566c4
phongcheat-phus <phongcheat.phus@gmail.com>
2025-02-19 09:20:11 +0700

    delete code function read_item and create_item

--- api/main.py
+++ api/main.py
@@ -9,10 +9,3 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, World!"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/items/")
-def create_item(item: dict): 
-    return {"message": "item created", "item": item}
