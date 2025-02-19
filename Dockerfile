4cb933b8ec420619df53ea490de01f454f729838
root <root@ip-172-31-15-60.ap-southeast-1.compute.internal>
2025-02-19 04:03:08 +0000

    chage api dockerfile api/main.py

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY api/ /app/
 EXPOSE 8000
 
 # running api server 
-# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- api/main.py
+++ api/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
